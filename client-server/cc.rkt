f1a0177c028724664e9b1bce452093ebc64cf2cf
Brett Stahlman <brett.p.stahlman@saic.com>
2014-09-12 08:19:27 -0500

    Got login partially working.

--- client-server/cc.rkt
+++ client-server/cc.rkt
@@ -2,18 +2,27 @@
 
 (require racket/tcp)
 (require rnrs/io/ports-6)
+(require racket/string) ; string-trim
 
 (printf "Trying to connect to server... ")
 (define-values (in out) (tcp-connect "localhost" 12346))
 (printf "Ok.\n")
 
-(define (handle-server-msg p)
-  (let* ((msg (read p))
-	 (q (equal? msg 'quit)))
-    (when (not q)
-      (displayln msg) (flush-output))
-    ; Return port as sync result
-    p))
+(define (process-server-msg msg)
+  ; TODO!!
+  (printf "Client received: `~a'\n" msg)
+  (flush-output))
+
+; TODO: Fix this - like server!
+; Eventually, move into common...
+(define (handle-server-msg in)
+  (printf "Inside handle-server-msg!\n") (flush-output)
+  (define spc (peek-string 1 0 in))
+  (printf "Just peeked string\n `~a'" spc) (flush-output)
+  (if (string=? "" (string-trim spc))
+    (begin (printf "About to read-string\n") (flush-output) (read-string 1 in) "")
+    (begin (printf "About to read form\n") (flush-output)
+	   (process-server-msg (read in)))))
 
 (define (handle-rdy-to-send p)
   (when (byte-ready? p)
--- client-server/cs.rkt
+++ client-server/cs.rkt
@@ -4,35 +4,59 @@
 (require racket/tcp)
 (require racket/string)
 
-(struct client (username in out logged-in))
-(define client-ios '())
+(struct client (name in out) #:mutable)
 (define clients (hash))
 
 (define (handle-new-client io)
-  ; Add '(in out) to list of clients
-  ; Separately, maintain a hash keyed by the in port itself.
-  (set! client-ios (cons io client-ios))
+  ; Maintain a hash keyed by the in port itself.
   (set! clients (hash-set clients
 			  (car io)
-			  (client "" (car io) (cdr io) #f)))
+			  (client #f (car io) (cadr io))))
   (printf "Client connected!\n")
-  (flush-output)
-  ; Return ports as sync result
-  io)
+  (flush-output))
 
-;(define (handle-client-msg port)
-;  (printf "Client msg received: ~a!\n" (read port)) (flush-output)
-;  ; Leave "synchronization result" unchanged: i.e., return the port.
-;  port)
+; Send to all logged-in clients but the specified one.
+(define (bcast-client-msg msg cli)
+  (for* [(o-cli clients)
+	 (out (client-out o-cli))
+	 #:unless (eq? cli o-cli)
+	 #:when (client-name o-cli)]
+	(write msg out)
+	(flush-output out)))
+
+; TODO: Consider turning this into a read-and-process-datum function, which takes only in port.
+(define (process-client-msg msg cli)
+  (define out (client-out cli))
+  (case (car msg)
+    ; Handle client login
+    [(login)
+     (printf "Got login request for ~a\n" (cadr msg)) (flush-output)
+     (let* [(name (cadr msg))
+	    ; Recall name starts out #f before login.
+	    (rsp (not (findf (lambda (c) (and (client-name c)
+					      (string=? (client-name c) name))) (hash-values clients))))]
+       (when rsp
+	 (set-client-name! cli (cadr msg)))
+       ; Ether way, send client response: accept or reject
+       (write (list 'login rsp) out)
+       (flush-output out)
+       (printf "Wrote accept/reject back to client!\n")
+       (flush-output))]
+    [(msg)
+     (bcast-client-msg msg cli)]
+    [else
+      (printf "Bad msg from client! ~a - ~a\n" msg (car msg))]))
 
 (define (handle-client-msg in)
   (printf "Inside handle-client-msg!\n") (flush-output)
+  ; Consider using peek-bytes-avail! here to ensure that strange activity on port doesn't cause us to hang.
   (define spc (peek-string 1 0 in))
   (printf "Just peeked string\n `~a'" spc) (flush-output)
-  (define d (if (string=? "" (string-trim spc))
-	      (begin (printf "About to read-string\n") (flush-output) (read-string 1 in) "")
-	      (begin (printf "About to read form\n") (flush-output) (read in))))
-  (printf "Client msg received: `~a'!\n" d) (flush-output))
+  (if (string=? "" (string-trim spc))
+    (begin (printf "About to read-string\n") (flush-output) (read-string 1 in)
+	   (printf "Just read string\n") (flush-output) "")
+    (begin (printf "About to read form\n") (flush-output)
+	   (process-client-msg (read in) (hash-ref clients in)))))
 
 (define listener (tcp-listen 12346))
 (define new-client-evt (wrap-evt (tcp-accept-evt listener) handle-new-client))
@@ -40,7 +64,6 @@
 (let loop ()
   ; Note: sync res for new-client-evt is actually 2 el list of in out
   (apply sync new-client-evt
-	 ;(map (lambda (c) (wrap-evt (client-in c) handle-client-msg)) (hash-values clients)))
 	 (map (lambda (p) (wrap-evt p handle-client-msg)) (hash-keys clients)))
 
   (loop))
